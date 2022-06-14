457d4b9f353cb2ff5bb721a741f2ad318459ce9d
Junico Tandiago <junicotandiagoo21@gmail.com>
2022-06-14 09:59:53 +0700

    integration landing page, checkout, and my dashboard

--- resources/views/checkout/success.blade.php
+++ resources/views/checkout/success.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 
-@section('component')
+@section('content')
 <section class="checkout">
     <div class="container">
         <div class="row text-center">
             <div class="col-lg-12 col-12">
                 <img src="{{ asset('images/ill_register.png') }}" height="400" class="mb-5" alt=" ">
             </div>
-            <div class=" col-lg-12 col-12 header-wrap mt-4">
+            <div class=" col-lg-12 col-12 header-wrap mt-4" alt="ok">
                 <p class="story">
                     WHAT A DAY!
                 </p>
                 <h2 class="primary-header ">
                     Berhasil Checkout
                 </h2>
-                <a href="#" class="btn btn-primary mt-3">
+                <a href="{{ route('dashboard') }}" class="btn btn-primary mt-3">
                     My Dashboard
                 </a>
             </div>
